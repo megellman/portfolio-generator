a9d1bd007d4ff510c2ca0e5da8588b448d39a1bd
Megan Ellman <meganlellman@gmail.com>
2023-01-23 12:36:17 -0800

    added styling

--- assets/script.js
+++ assets/script.js
@@ -29,20 +29,29 @@ inquirer.prompt([{
 });
 
 function createPort(response){
-fs.writeFile('index2.html', `<!DOCTYPE html>
+fs.writeFile('index.html', `<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portfolio</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,200;1,100;1,200&family=Yeseva+One&display=swap" rel="stylesheet">
 </head>
 <body>
     <h1 id="name">${response.name}</h1>
-    <h2 id="location">${response.location}</h2>
-    <h2 id="bio">${response.bio}</h2>
-    <h2 id="linkedin">${response.linkedIn}</h2>
-    <h2 id="github">${response.github}</h2>
+    <h2 id="location">Location: ${response.location}</h2>
+    <h2 id="bio">A little about me:</h2>
+    <p>${response.bio}</p>
+    <a href="${response.linkedIn}">
+        <h2 id="linkedin">LinkedIn</h2>
+    </a>
+    <a href="${response.github}">
+        <h2 id="github">GitHub</h2>
+    </a>
 </body>
 </html>`, function(err){
     if(err){
